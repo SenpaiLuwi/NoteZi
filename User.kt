e40c2a469361eba5b730da41bdf32fc1acaed662
John Louie Ubias <110644790+SenpaiLuwi@users.noreply.github.com>
2023-12-03 16:20:56 +0800

    Delete User.kt

--- User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.notezi
-
-data class User(
-    val userName: String = "",
-    val userEmail: String = "",
-    val userSchoolID: String = "",
-    val userProfileImageUri: String = ""
-)
