61a1b24c3686497907ffeee0530c2038b088871c
Rezenro <116412689+rezenro23@users.noreply.github.com>
2023-12-03 14:05:24 +0800

    Update build.gradle.kts - Rotsen

--- NoteZi/app/build.gradle.kts
+++ NoteZi/app/build.gradle.kts
@@ -46,6 +46,12 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.orhanobut:dialogplus:1.11@aar")
 
+    //INPUTED BY ROTSEN
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
